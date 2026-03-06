e1b909e541d2962a2c1501632780f5fb04581503
neweideas <candicenelms@gmail.com>
2026-03-06 00:53:09 -0500

    fix supabase key

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { createClient } from "@supabase/supabase-js";
 
 const SUPABASE_URL = "https://gjujjdmpxowzosnlrcum.supabase.co";
-const SUPABASE_KEY = "sb_publishable_GnReOIrnCrHNbNiNBX8Vxw_rm0eGK2H";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdqdWpqZG1weG93em9zbmxyY3VtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI3NzA5OTMsImV4cCI6MjA4ODM0Njk5M30.cqgdBE_qOe0GIyhamBp7HHBC9Knb0oeqwQGsy62if0s";
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 const USER_ID = "student"; // student's fixed ID
 const PARENT_ID = "parent"; // your view ID
