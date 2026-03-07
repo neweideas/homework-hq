95b1f1f6617a39f00f2f03b1053ec5e25c55212e
Candice Nelms <candicenelms@gmail.com>
2026-03-07 14:25:58 -0500

    fix: move supabase credentials to env variables

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { useState, useEffect } from "react"; 
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "https://gjujjdmpxowzosnlrcum.supabase.co";
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdqdWpqZG1weG93em9zbmxyY3VtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI3NzA5OTMsImV4cCI6MjA4ODM0Njk5M30.cqgdBE_qOe0GIyhamBp7HHBC9Knb0oeqwQGsy62if0s";
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 const USER_ID = "student";
 
